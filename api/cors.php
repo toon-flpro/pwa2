347575664801994d63a61ac86fa8dccdf619e891
PC\toon1 <toon.flpro@gmail.com>
2025-01-13 09:43:08 +0700

    4

--- api/cors.php
+++
@@ -1,7 +0,0 @@
-<?php
-//header("Access-Control-Allow-Origin: *"); // Hide or restrict for production
-//header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT"); // Hide or restrict for production
-//header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Hide or restrict for production
-
-date_default_timezone_set('Asia/Singapore');//Do not comment!
-?>
